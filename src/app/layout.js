db220eb29de97e7c7f188931ae15d31e22caf39a
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-08-30 16:43:02 +0530

    d

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,19 @@ import "./globals.css";
 export const metadata = {
   title: "Upflair",
   description: "Empowering your Potential, Energising your Growth",
+
+
 };
+
+export const favicon = { favicon  : "./favicon.png",};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-  
-      >
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
+      <body>
         {children}
       </body>
     </html>
